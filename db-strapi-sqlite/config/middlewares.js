4d735867d604c10a840188384924bf6e9b24faf9
Dogron <fedor.ryzhenkov@gmail.com>
2024-03-26 18:06:24 +0400

    Add strapi middlewares

--- db-strapi-sqlite/config/middlewares.js
+++ db-strapi-sqlite/config/middlewares.js
@@ -1,12 +1,1 @@
-export default [
-  'db-strapi-sqlite::logger',
-  'db-strapi-sqlite::errors',
-  'db-strapi-sqlite::security',
-  'db-strapi-sqlite::cors',
-  'db-strapi-sqlite::poweredBy',
-  'db-strapi-sqlite::query',
-  'db-strapi-sqlite::body',
-  'db-strapi-sqlite::session',
-  'db-strapi-sqlite::favicon',
-  'db-strapi-sqlite::public',
-];
+
