e917cbb2b549927c3061e63914a123ae7e4595f1
Dogron <fedor.ryzhenkov@gmail.com>
2024-03-27 13:04:57 +0400

    Test

--- db-strapi/config/env/production/database.js
+++ db-strapi/config/env/production/database.js
@@ -1,16 +1,2 @@
 
-module.exports = ({ env }) => ({
-  connection: {
-    host: env('DATABASE_HOST', 'localhost'),
-    port: env.int('DATABASE_PORT', 5432),
-    database: env('DATABASE_NAME', 'postgres'),
-    user: env('DATABASE_USERNAME', 'postgres'),
-    password: env('DATABASE_PASSWORD', '0000'),
-    sslmode: 'disable',
-    ssl: {
-      rejectUnauthorized: false,
-    },
-    schema: env('DATABASE_SCHEMA', 'public'),
-  },
-  pool: {min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10)},
-});
+
