2c69b39843a1e7384f89f7f06dbc9550c47017c0
Dogron <fedor.ryzhenkov@gmail.com>
2024-03-27 12:59:13 +0400

    Possible fix to ssl-self-signed error 3 (unsafe for production)

--- db-strapi/config/env/production/database.js
+++ db-strapi/config/env/production/database.js
@@ -1,18 +1,13 @@
 
 module.exports = ({ env }) => ({
   connection: {
-    connectionString: env('DATABASE_URL'),
     host: env('DATABASE_HOST', 'localhost'),
     port: env.int('DATABASE_PORT', 5432),
     database: env('DATABASE_NAME', 'postgres'),
     user: env('DATABASE_USERNAME', 'postgres'),
     password: env('DATABASE_PASSWORD', '0000'),
-    ssl: env.bool('DATABASE_SSL', false) && {
-      key: env('DATABASE_SSL_KEY', undefined),
-      cert: env('DATABASE_SSL_CERT', undefined),
-      ca: env('DATABASE_SSL_CA', undefined),
-      capath: env('DATABASE_SSL_CAPATH', undefined),
-      cipher: env('DATABASE_SSL_CIPHER', undefined),
+    sslmode: 'disable',
+    ssl: {
       rejectUnauthorized: false,
     },
     schema: env('DATABASE_SCHEMA', 'public'),
