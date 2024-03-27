53e10faff582ec7735dbf5656fba20bd5ea4a4d5
Dogron <fedor.ryzhenkov@gmail.com>
2024-03-27 13:35:18 +0400

    Reverted changes

--- db-strapi/config/env/production/database.js
+++ db-strapi/config/env/production/database.js
@@ -1,25 +1,18 @@
 
 module.exports = ({ env }) => ({
-  postgres: {
+  connection: {
+    client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '0000'),
-      ssl: env.bool('DATABASE_SSL', false) && {
-        key: env('DATABASE_SSL_KEY', undefined),
-        cert: env('DATABASE_SSL_CERT', undefined),
-        ca: env('DATABASE_SSL_CA', undefined),
-        capath: env('DATABASE_SSL_CAPATH', undefined),
-        cipher: env('DATABASE_SSL_CIPHER', undefined),
-        rejectUnauthorized: env.bool(
-          'DATABASE_SSL_REJECT_UNAUTHORIZED',
-          false
-        ),
+      sslmode: 'disabled',
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
       },
-      schema: env('DATABASE_SCHEMA', 'public'),
     },
-    pool: {min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10)},
+    debug: false,
   },
 });
